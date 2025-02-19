2ccf6aa96e3f63ef8997c9a4a94bcf2663bd15b1
Philipp Schmoll <philipp.schmoll@gmx.de>
2025-02-19 13:32:40 +0100

    fixes to make tests run

--- src/HTTN.jl
+++ src/HTTN.jl
@@ -68,7 +68,7 @@ export constructPhysSpaces, constructVirtSpaces, getLinkDimsMPS, getLinkDimsMPO
 export infimum_larger_deg
 export diagTM
 export transform_basis!, sample_MPS!, sample_MPS_block!, sample_to_BPS, sample_to_CPS
-export save_to_file, load_from_file
+# export save_to_file, load_from_file
 export compute_average
 
 # export Bogoliubov transformation functions
